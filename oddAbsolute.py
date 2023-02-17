bbc90ee7897508dab71c3f405425600e1f4fa6fc
Forest Nenninger <forestnenninger@Forests-MacBook-Pro.local>
2023-02-16 19:00:16 -0600

    defined input validation func and calculation func

--- oddAbsolute.py
+++ oddAbsolute.py
@@ -1,7 +1,20 @@
+def inputValidation():
+    while True:
+        try:
+            in_num  = int(input("Enter a number: "))
+            break
+        except:
+            print("Please enter only numeric values, without any text or special characters. Please try again.")
+            continue
+    return in_num
+
 def calculateAbsolute():
-    
     # This first line is provided for you
-    in_num  = input("Enter a number: ")
+    n=inputValidation()
+    if n > 21: difference = (n-21)*2
+    else: difference = n - 21
+    if difference < 0: difference = difference * -1
+    print (f'Result: {difference}')
     # end assignment
 
 ## If you want to test locally run > python payCalculator.py
